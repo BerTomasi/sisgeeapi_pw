17239cb948f13d26f0518c0b3165fd0f95acf081
Bernardo Dirceu Tomasi <20202PF.CC0002@passofundo.ifsul.local>
2022-11-29 08:15:25 -0300

    29/11

--- rotas.js
+++ rotas.js
@@ -1,16 +1,27 @@
 const { Router } = require('express');
 
 const controlePredios = require('./controladores/predios');
+const controleSalas = require("./controladores/salas");
 
 const rotas = new Router();
 
 rotas.route('/predios')
-    .get(controlePredios.getPredios)
-    .post(controlePredios.addPredios)
-    .put(controlePredios.updatePredios)
+     .get(controlePredios.getPredios)
+     .post(controlePredios.addPredios)
+     .put(controlePredios.updatePredios)
 
 rotas.route('/predios/:codigo')
-    .delete(controlePredios.deletePredios)
-    .get(controlePredios.getPrediosPorCodigo)
+     .delete(controlePredios.deletePredio)
+     .get(controlePredios.getPredioPorCodigo)
+
+
+rotas.route('/salas')
+   .get(controleSalas.getSalas)
+   .post(controleSalas.addSala)
+   .put(controleSalas.updateSala)
+
+rotas.route('/salas/:codigo')
+   .get(controleSalas.getSalaPorCodigo)
+   .delete(controleSalas.deleteSala)
 
 module.exports = rotas;
